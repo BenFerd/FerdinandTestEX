635e287d25a939920ae36d70ccdd838b1d817d89
Ben <ben.crommen@gmail.com>
2020-10-07 10:33:33 +0200

    html + gulp + css  + btn search

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 const { src, dest, watch } = require("gulp");
-
 const compileSass = require("gulp-sass");
+compileSass.compiler = require("node-sass");
+const concat = require("gulp-concat");
 
-compileSass.compiler = require('node-sass');
 
 const bundleSass = () => {
-    return src('./css/*.scss')
-        .pipe(compileSass().on('error', compileSass.logError))
-        .pipe(dest('./css'));
+  return src("./css/*.scss")
+    .pipe(compileSass().on("error", compileSass.logError))
+    .pipe(concat("style.css"))
+    .pipe(dest("css"));
 };
 
 const devWatch = () => {
-    watch('./css/*.scss', bundleSass);
+  watch("./css/*.scss", bundleSass);
 };
 
 module.exports = {
-  bundleSass, devWatch,
+  bundleSass,
+  devWatch,
 };
